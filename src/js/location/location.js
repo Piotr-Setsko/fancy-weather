f127c5ed90789e74e4ecb236c79e4e88d2aefe4a
Piotr-Setsko <lihadzey@gmail.com>
2021-05-06 21:42:34 +0300

    feat: add names.json

--- src/js/location/location.js
+++ src/js/location/location.js
@@ -1,14 +1,15 @@
-async function getUserLocation() {
+const countryName = import('../../assets/names.json')
+
+function getUserLocation() {
   const LOCATION_API_TOKEN = 'b319f7092b2b9b';
 
   return fetch(`https://ipinfo.io/json?token=${LOCATION_API_TOKEN}`)
     .then((response) => response.json());
 }
 
-async function getCountryName(country) {
-  return fetch('https://cors-anywhere.herokuapp.com/http://country.io/names.json')
-    .then((response) => response.json())
-    .then((data) => data[`${country}`]);
+function getCountryName(country) { 
+  return countryName          
+          .then((data) => data[`${country}`]);    
 }
 
 export { getUserLocation, getCountryName };
