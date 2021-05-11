2b07837b2755df1811694319ee59616ae5d54ef3
Piotr-Setsko <lihadzey@gmail.com>
2021-05-11 17:09:18 +0300

    fix: rename utilities to date

--- src/index.js
+++ src/index.js
@@ -2,46 +2,19 @@ import './css/style.css';
 import '@babel/polyfill';
 import { renderForecastInfo } from './js/dom/dom';
 import init from './js/map/map';
-import { getTime } from './js/utilities/utilities';
+import { getTime } from './js/date/date';
 import { checkLang, translateCountryName } from './js/translate/translate';
 import { weatherData, unitCheck } from './js/weather/weather';
 import { getImage } from './js/background/background';
 import { searchCity } from './js/search/search';
 import './js/eventListeners/eventListeners';
 
-import { setCORS } from "google-translate-api-browser";
-const translate = setCORS("http://cors-anywhere.herokuapp.com/");
-
-const language = import('./assets/language.json').then(({ default: lang }) => lang);
-
-const selectLang = document.querySelector('.control__select-lang');
-
-let weatherInfo;
-
 const create = async () => {
-  try {
     let unit = unitCheck();
     const search = await searchCity();
-    //const { ru, en } = await language;
-
     let { lang, langLetter } = await checkLang();
 
-
-/*
-    if (sessionStorage.lang === undefined || sessionStorage.lang === 'en') {
-      sessionStorage.lang = 'en';
-      lang = en;
-      langLetter = 'en';
-    } else {
-      document.children[0].lang = sessionStorage.lang;
-        lang = ru;
-        langLetter = 'ru';
-    }
-    */
-
-    //document.querySelector('.control__select-lang').value = sessionStorage.lang;
-
-    weatherInfo = await weatherData(search, unit, langLetter);
+    let weatherInfo = await weatherData(search, unit, langLetter);
 
     const { timezone } = weatherInfo;
 
@@ -61,65 +34,10 @@ const create = async () => {
     let data = await getTime(timezone, weatherInfo.nextDays);
 
     weatherInfo.countryName = await translateCountryName(weatherInfo.countryName);
-/*
-    if (sessionStorage.lang !== 'en') {
-      const transl = await translate(`${weatherInfo.countryName}`, {from:'en', to: `${sessionStorage.lang}`});
-      let { text } = transl;
-      //const textStr = text.split(',');
-      weatherInfo.countryName = text;
-    }
-   */
-    renderForecastInfo(data, weatherInfo, lang);
 
+    renderForecastInfo(data, weatherInfo, lang);
     init(weatherInfo.coord);
 
-    selectLang.addEventListener('change', async (event) => {
-      const { ru, en } = await language;
-      if (event.target.value === 'ru') {
-        sessionStorage.setItem('lang', 'ru');
-        lang = ru;
-        document.children[0].lang = 'ru';
-
-        weatherInfo = await weatherData(search, unit, event.target.value);
-
-        const transl = await translate(`${weatherInfo.countryName}`, {from: 'en', to: 'ru'});
-        console.log(transl);
-        let { text } = transl;
-        //const textStr = text.split(',');
-        weatherInfo.countryName = text;
-
-        data = await getTime(timezone, weatherInfo.nextDays);
-      }
-     /* else if (event.target.value === 'be') {
-        sessionStorage.setItem('lang', 'be');
-        lang = be;
-        document.children[0].lang = 'be';
-
-        weatherInfo = await weatherData(search, unit, event.target.value);
-        data = await getTime(timezone, weatherInfo.nextDays);
-
-        const transl = await translate(`${weatherInfo.name}, ${weatherInfo.countryName}, ${weatherInfo.description}`, {to: "be"});
-        let { text } = transl;
-        const textStr = text.split(',');
-        [weatherInfo.name, weatherInfo.countryName, weatherInfo.description] = textStr;
-      } */
-      else {
-        sessionStorage.setItem('lang', 'en');
-        lang = en;
-        document.children[0].lang = 'en';
-
-        weatherInfo = await weatherData(search, unit, event.target.value);
-        data = await getTime(timezone, weatherInfo.nextDays);
-      }
-
-      document.querySelector('.weather__wrapper').remove();
-      document.querySelector('.location__map').remove();
-      renderForecastInfo(data, weatherInfo, lang);
-      init(weatherInfo.coord);
-    });
-  } catch (e) {
-    // console.log(e);
-  }
 }
 
 create();
--- src/js/eventListeners/eventListeners.js
+++ src/js/eventListeners/eventListeners.js
@@ -1,13 +1,19 @@
 import {searchCity} from '../search/search';
 import create from '../../index';
 import { weatherData, unitCheck } from '../weather/weather';
+import { translateCountryName } from '../translate/translate';
+import { getTime } from '../date/date';
+import { renderForecastInfo } from '../dom/dom';
+import init from '../map/map';
 
 const refresh = document.querySelector('.control__refresh');
 const btn = document.getElementById('btn');
 const metric = document.querySelector('.control__unit--metric');
 const imperial = document.querySelector('.control__unit--imperial');
 const unitToggle = document.querySelector('.control__select-unit');
+const selectLang = document.querySelector('.control__select-lang');
 
+const language = import('../../assets/language.json').then(({ default: lang }) => lang);
 
 
 refresh.addEventListener('click', () => {
@@ -42,3 +48,36 @@ unitToggle.addEventListener('click', async () => {
     document.querySelectorAll('.weather-future__temp')[i].innerHTML = `${Math.round(weatherInfo.tempTomorrow[i])}<span>&deg;</span>`;
   }
 });
+
+selectLang.addEventListener('change', async (event) => {
+  const { ru, en } = await language;
+  let lang, data;
+  let weatherInfo;
+  let unit = unitCheck();
+  const search = await searchCity();
+
+  if (event.target.value === 'ru') {
+    sessionStorage.setItem('lang', 'ru');
+    lang = ru;
+    document.children[0].lang = 'ru';
+
+    weatherInfo = await weatherData(search, unit, event.target.value);
+    weatherInfo.countryName = await translateCountryName(weatherInfo.countryName);
+
+  } else {
+    sessionStorage.setItem('lang', 'en');
+    lang = en;
+    document.children[0].lang = 'en';
+
+    weatherInfo = await weatherData(search, unit, event.target.value);
+  }
+
+    const { timezone } = weatherInfo;
+    data = await getTime(timezone, weatherInfo.nextDays);
+
+    document.querySelector('.weather__wrapper').remove();
+    document.querySelector('.location__map').remove();
+
+    renderForecastInfo(data, weatherInfo, lang);
+    init(weatherInfo.coord);
+  });
